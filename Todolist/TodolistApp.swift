3338d6dbb7b4a163ef2c1f4eebc90e43405dc2f2
zhangyu1818 <723156735@qq.com>
2020-12-24 14:13:48 +0800

    Initial Commit

---
+++ Todolist/TodolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodolistApp.swift
+//  Todolist
+//
+//  Created by ZhangYu on 2020/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TodolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
